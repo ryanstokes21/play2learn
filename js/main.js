e3efe25d0cb17ad24852a1846ee426fa7c37630b
Ryan Stokes <ryanstokescfbre@gmail.com>
2026-05-31 14:09:20 -0500

    add start menu function and creating math problems

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 import showTestimonial from './quotes.js';
+import { selectOperation, createMathProblem } from './math-facts.js';
+
+const gameBtn = document.getElementById('game-btn');
+const mathFactsStartMenu = document.getElementById('math-facts-start-menu');
+const mathFactsGameBoard = document.getElementById('math-facts-game-board');
+const operationSelect = document.getElementById('operation');
+
+if (gameBtn) {
+  gameBtn.addEventListener('click', () => {
+    mathFactsStartMenu.classList.toggle('hidden');
+    mathFactsGameBoard.classList.toggle('hidden');
+    selectOperation(operationSelect);
+    const { problem } = createMathProblem(operationSelect.value);
+
+    document.getElementById('problem').textContent = problem;
+  });
+}
 
 showTestimonial();
 setInterval(showTestimonial, 10000);
